d3c93d9c68449735b65fe93a37f05001ab755d19
Weronika Szeszol <weronikaszeszol@gmail.com>
2025-04-26 10:15:08 +0200

    Mppper Correction

--- wsb-jpa1/src/main/java/com/jpacourse/mapper/PatientMapper.java
+++ wsb-jpa1/src/main/java/com/jpacourse/mapper/PatientMapper.java
@@ -1,5 +1,7 @@
 package com.jpacourse.mapper;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import com.jpacourse.dto.PatientTO;
@@ -13,45 +15,39 @@ public final class PatientMapper {
             return null;
         }
         
-        final PatientTO patientTO = new PatientTO();
-        patientTO.setId(patientEntity.getId());
-        patientTO.setFirstName(patientEntity.getFirstName());
-        patientTO.setLastName(patientEntity.getLastName());
-        patientTO.setTelephoneNumber(patientEntity.getTelephoneNumber());
-        patientTO.setEmail(patientEntity.getEmail());
-        patientTO.setPatientNumber(patientEntity.getPatientNumber());
-        patientTO.setDateOfBirth(patientEntity.getDateOfBirth());
-        patientTO.setDateOfRegistration(patientEntity.getRegistrationDate());
-        
-        // if (patientEntity.getAddress() != null) {
-        //     patientTO.setAddressId(patientEntity.getAddress().getId());
-        // }
-        
-        if (patientEntity.getVisits() != null) {
-            patientTO.setVisits(
-                patientEntity.getVisits().stream()
-                    .map(visit -> VisitMapper.mapToTO(visit))
-                    .collect(Collectors.toList())
-            );
-        }
-        
-        return patientTO;
-    }
+        List<VisitTO> visits = patientEntity.getVisits() != null ?
+        patientEntity.getVisits().stream()
+            .map(VisitMapper::mapToTO)
+            .collect(Collectors.toList()) :
+        new ArrayList<>();
+    
+    return new PatientTO(
+        patientEntity.getId(),
+        patientEntity.getFirstName(),
+        patientEntity.getLastName(),
+        patientEntity.getTelephoneNumber(),
+        patientEntity.getEmail(),
+        patientEntity.getPatientNumber(),
+        patientEntity.getDateOfBirth(),
+        patientEntity.getRegistrationDate(),
+        visits
+    );
+}
 
-    public static PatientEntity mapToEntity(final PatientTO patientTO) {
-        if (patientTO == null) {
-            return null;
-        }
-        
-        final PatientEntity patientEntity = new PatientEntity();
-        patientEntity.setId(patientTO.getId());
-        patientEntity.setFirstName(patientTO.getFirstName());
-        patientEntity.setLastName(patientTO.getLastName());
-        patientEntity.setTelephoneNumber(patientTO.getTelephoneNumber());
-        patientEntity.setEmail(patientTO.getEmail());
-        patientEntity.setPatientNumber(patientTO.getPatientNumber());
-        patientEntity.setDateOfBirth(patientTO.getDateOfBirth());
-        
-        return patientEntity;
+public static PatientEntity mapToEntity(final PatientTO patientTO) {
+    if (patientTO == null) {
+        return null;
     }
+    
+    final PatientEntity patientEntity = new PatientEntity();
+    patientEntity.setId(patientTO.getId());
+    patientEntity.setFirstName(patientTO.getFirstName());
+    patientEntity.setLastName(patientTO.getLastName());
+    patientEntity.setTelephoneNumber(patientTO.getTelephoneNumber());
+    patientEntity.setEmail(patientTO.getEmail());
+    patientEntity.setPatientNumber(patientTO.getPatientNumber());
+    patientEntity.setDateOfBirth(patientTO.getDateOfBirth());
+    
+    return patientEntity;
+}
 }

138d67be822c7e7fc1d01181d2e680665980d87d
Weronika Szeszol <weronikaszeszol@gmail.com>
2025-05-02 18:08:17 +0200

    VisitServiceTest dziala

--- wsb-jpa1/src/test/java/com/jpacourse/persistance/dao/VisitServiceTest.java
+++ wsb-jpa1/src/test/java/com/jpacourse/persistance/dao/VisitServiceTest.java
@@ -10,7 +10,6 @@ import com.jpacourse.service.VisitService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
